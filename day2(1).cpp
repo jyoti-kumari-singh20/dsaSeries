a6e5035f907306d44fa77abf7cd8e095fdf40e1d
jyoti-kumari-singh20 <jyotikumarisingh881@gmail.com>
2025-03-04 10:34:51 +0530

    added day-problem-1.cpp

--- day2(1).cpp
+++ day2(1).cpp
@@ -1,22 +1,34 @@
 #include<bits/stdc++.h>
 using namespace std;
 class Solution {
-  public:
-      vector<vector<int>> mergeArrays(vector<vector<int>>& nums1, vector<vector<int>>& nums2) {
-          int l1=nums1.size();
-          int l2=nums2.size();
-          vector<vector<int>> ans;
-          for(int i=0;i<l1;i++){
-              for(int j=0;j<l2;j++){
-                  if(nums1[i][0]==nums2[j][0]){
-                      ans[i][0]=nums1[i][0];
-                      ans[i][1]=nums1[i][1]+nums2[j][1];
-                  }
-                  else{
-                      ans.push_back(nums1[i]);
-                  }
-              }
-          }
-          return nums1;
-      }
-  };
+    public:
+        vector<vector<int>> mergeArrays(vector<vector<int>>& nums1, vector<vector<int>>& nums2) {
+            int l1 = nums1.size();
+            int l2 = nums2.size();
+            vector<vector<int>> ans;
+            int i = 0, j = 0;
+            while (i < l1 && j < l2) {
+                if (nums1[i][0] < nums2[j][0]) {
+                    ans.push_back(nums1[i]);
+                    i++;
+                } else if (nums1[i][0] > nums2[j][0]) {
+                    ans.push_back(nums2[j]);
+                    j++;
+                } else {
+                    int val=nums1[i][1] + nums2[j][1];
+                    ans.push_back({nums1[i][0], val});
+                    i++;
+                    j++;
+                }
+            }
+            while (i < l1) {
+                ans.push_back(nums1[i]);
+                i++;
+            }
+            while (j < l2) {
+                ans.push_back(nums2[j]);
+                j++;
+            }
+            return ans;
+        }
+    };
