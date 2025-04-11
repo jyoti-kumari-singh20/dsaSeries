d590004830ae4777cb9376efa3e874f1deba2e48
jyoti-kumari-singh20 <jyotikumarisingh881@gmail.com>
2025-04-12 00:46:51 +0530

    created day30

---
+++ day30/1.cpp
@@ -0,0 +1,54 @@
+// Problem:Given the head of a linked list, return the list after sorting it in ascending order.
+
+// Code:
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+  public:
+          ListNode* merge(ListNode* l1, ListNode* l2){ 
+          ListNode temp(0);
+          ListNode* tail = &temp;
+          
+          while (l1 && l2) {
+              if (l1->val < l2->val) {
+                  tail->next = l1;
+                  l1 = l1->next;
+              } else {
+                  tail->next = l2;
+                  l2 = l2->next;
+              }
+              tail = tail->next;
+          }
+  
+          tail->next = l1 ? l1 : l2;
+          return temp.next;
+      }
+      ListNode* sortList(ListNode* head) {
+          if (!head || !head->next) 
+              return head;
+          ListNode* slow = head;
+          ListNode* fast = head;
+          ListNode* prev = nullptr;
+          while (fast && fast->next) {
+              prev = slow;
+              slow = slow->next;
+              fast = fast->next->next;
+          }
+          prev->next = nullptr;
+          ListNode* l1 = sortList(head);
+          ListNode* l2 = sortList(slow);
+          return merge(l1, l2);
+      }
+      
+  };
+
+// TC:O(nlogn)
+// SC:O(logn)
